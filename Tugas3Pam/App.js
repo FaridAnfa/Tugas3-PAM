f954d97ff6294d14dbdc82640b617df0e67dc117
FaridAnfa <91378970+FaridAnfa@users.noreply.github.com>
2023-04-17 14:55:17 +0700

    Delete Tugas3Pam directory

--- Tugas3Pam/App.js
+++
@@ -1,9 +0,0 @@
-//Main App untuk navigasi ke route
-import React from 'react'
-import Navigation from './Source/navigation/navigation'
-
-export default function App(){
-  return(
-    <Navigation/>
-  )
-}
--- Tugas3Pam/README.md
+++
@@ -1,9 +0,0 @@
-# RB-Farid Anfasha-Tugas3PAM
-
-## Identitas Diri:
-## Nama: Farid Anfasha 
-## Kelas:RB
-## Nim:119140215
-
-
-
